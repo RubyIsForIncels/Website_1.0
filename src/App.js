4d8a4bca63b494723ac1d461959e0b7898ac353e
RubyIsForIncels <97403033+RubyIsForIncels@users.noreply.github.com>
2022-04-05 09:58:50 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hi, my name is Carolus</h1>
+      <h2>Go get some bitches, I approve this message</h2>
+    </div>
+  );
+}
